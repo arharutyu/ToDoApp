cfcb53545477f185402fed66151c48348a40e0a9
Argine Harutyunyan <aharuty@live.com>
2024-03-07 18:03:13 +1100

    add id, update method for marking task complete

--- ToDoApp/Form1.cs
+++ ToDoApp/Form1.cs
@@ -24,6 +24,7 @@ namespace ToDoApp
             tasksListView.View = View.Details;
             tasksListView.FullRowSelect = true;
             tasksListView.Columns.Add("Task", 100);
+            tasksListView.Columns.Add("Id", 0);
             tasksListView.Columns.Add("Description", 200);
             tasksListView.Columns.Add("Due Date", 100);
 
@@ -49,51 +50,26 @@ namespace ToDoApp
             task.completed = false;
 
             mySchedule.tasks.Add(task);
-
-            // Temporarily detach the event handler to avoid automatic selection
-            tasksListView.SelectedIndexChanged -= tasksListView_SelectedIndexChanged;
-
             UpdateTasksListView();
-
-            // Reattach the event handler
-            tasksListView.SelectedIndexChanged += tasksListView_SelectedIndexChanged;
 
             addTaskTextBox.Text = "";
             addTaskDescription.Text = "";
             addTaskDueDate.Value = DateTime.Now;
 
-            MessageBox.Show("Task added: " + taskText, "Task Added");
-        }
-
-        private bool isTaskOverdue(string dueDate)
-        {
-            DateTime currentDate = DateTime.Today;
-            string dateTimeFormat = "dd-MM-yyyy";
-
-            if (DateTime.TryParseExact(dueDate, dateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result))
-            {
-                return result < currentDate;
-            }
-            else
-            {
-                // Handle the case where the due date couldn't be parsed
-                throw new ArgumentException("Invalid due date format", nameof(dueDate));
-            }
+            //MessageBox.Show("Task added: " + taskText, "Task Added");
         }
 
         private void UpdateTasksListView()
         {
-            // Save the selected index before updating the data source
-            int selectedTaskIndex = tasksListView.SelectedIndices.Count > 0 ? tasksListView.SelectedIndices[0] : -1;
-
             tasksListView.Items.Clear();
             foreach (MyTask task in mySchedule.tasks.Where(task => !task.completed))
             {
                 ListViewItem item = new ListViewItem(task.name);
+                item.SubItems.Add(task.id.ToString());
                 item.SubItems.Add(task.description);
                 item.SubItems.Add(task.dueDate.ToString("dd-MM-yyyy"));
 
-                if (isTaskOverdue(task.dueDate.ToString("dd-MM-yyyy")))
+                if (task.isOverdue())
                 {
                     item.ForeColor = Color.Red;
                 }
@@ -101,14 +77,8 @@ namespace ToDoApp
                 tasksListView.Items.Add(item);
             }
 
-            // Restore the selection if it was saved
-            if (selectedTaskIndex != -1 && selectedTaskIndex < tasksListView.Items.Count)
-            {
-                tasksListView.Items[selectedTaskIndex].Selected = true;
-            }
-
             completedTasksListView.Items.Clear();
-            foreach (MyTask task in mySchedule.completedTasks)
+            foreach (MyTask task in mySchedule.tasks.Where(task => task.completed))
             {
                 ListViewItem item = new ListViewItem(task.name);
                 item.SubItems.Add(task.description);
@@ -118,45 +88,17 @@ namespace ToDoApp
             }
         }
 
-        private void tasksListView_SelectedIndexChanged(object sender, EventArgs e)
+        private void markComplete_Click(object sender, EventArgs e)
         {
-            // Display the confirmation dialog
-            using (var confirmationDialog = new ConfirmCompleteTask())
+            foreach (ListViewItem task in tasksListView.CheckedItems)
             {
-                var result = confirmationDialog.ShowDialog();
+                Guid guid = new Guid(task.SubItems[1].Text);
+                MyTask checkedTask = mySchedule.tasks.FirstOrDefault(m => m.id == guid);
 
-                // Check the result of the confirmation dialog
-                if (result == DialogResult.Yes)
-                {
-                    if (!updatingListView && tasksListView.SelectedItems.Count > 0)
-                    {
-                        ListViewItem selectedItem = tasksListView.SelectedItems[0];
-                        int selectedIndex = tasksListView.Items.IndexOf(selectedItem);
+                checkedTask.CompleteTask();
 
-                        MyTask selectedTask = mySchedule.tasks
-                            .Where(task => !task.completed)
-                            .ElementAtOrDefault(selectedIndex);
-
-                        if (selectedTask != null && !selectedTask.completed)
-                        {
-                            // Mark the selected task as complete
-                            selectedTask.CompleteTask();
-
-                            // Move the task from tasks to completedTasks
-                            mySchedule.tasks.Remove(selectedTask);
-                            mySchedule.completedTasks.Add(selectedTask);
-
-                            // Update the data binding
-                            updatingListView = true;
-                            UpdateTasksListView();
-                            updatingListView = false;
-                        }
-                    }
-                }
-                // If "Cancel" is clicked, do nothing
+                UpdateTasksListView();
             }
         }
-
     }
-
 }
